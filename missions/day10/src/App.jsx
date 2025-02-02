c481bdf1f24461dbd6c6705cf25c2340fcda15a9
jazzyfact <jazzyfact2000@gmail.com>
2025-02-02 22:15:52 +0900

    add:미션 9~10일차

---
+++ missions/day10/src/App.jsx
@@ -0,0 +1,11 @@
+import Welcome from "./Welcome.jsx";
+
+function App(){
+    return (
+        <div>
+            <Welcome name="이정환" isMember={true}/>
+        </div>
+    )
+}
+
+export default App;
